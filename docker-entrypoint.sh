db29cbf07ac409815c18b56fd90de747bb0f7e81
Kilsoo Kang <kilsookang@sk.com>
2017-08-14 02:12:38 +0900

    no message

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,11 @@ set -e
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
     set -- haproxy "$@"
+    echo "$@"
+    echo "11111111111111"
 fi
+
+echo "$@"
+echo "22222222222222"
 
 exec "$@"
