0185fbb30fee14a725b060c4071d90c17049be34
Kilsoo Kang <kilsookang@sk.com>
2017-08-14 01:54:34 +0900

    no message

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,10 +6,4 @@ if [ "${1#-}" != "$1" ]; then
     set -- haproxy "$@"
 fi
 
-if [ "$1" = 'haproxy' ]; then
-    # if the user wants "haproxy", let's use "haproxy-systemd-wrapper" instead so we can have proper reloadability implemented by upstream
-    #shift # "haproxy"
-    set -- haproxy-systemd-wrapper -p /run/haproxy.pid "$@"
-fi
-
 exec "$@"
